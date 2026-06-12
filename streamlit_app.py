c0164c3ba29452b037a96696c74548a449929f17
S.Widjaja <hi.amswidjaja@gmail.com>
2026-06-13 01:11:21 +0700

    fix embeddings device issue

--- streamlit_app.py
+++ streamlit_app.py
@@ -224,10 +224,7 @@ prompt = PromptTemplate(
 # =====================================================
 # EMBEDDING
 # =====================================================
-embedding = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2",
-    model_kwargs={"device": "cpu"}
-)
+embedding = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
 # =====================================================
